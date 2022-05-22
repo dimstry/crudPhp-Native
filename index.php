c93d0cca190032ac2826915af869b306d3a25940
GTM-Winter <dimastriana03@gmail.com>
2022-05-22 08:14:21 +0700

    add boostrap

--- index.php
+++ index.php
@@ -8,9 +8,43 @@ echo "
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <title>Crud</title>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css'>
   </head>
   <body>
-    <h1>Hello Word</h1>
+    <h1 class='text-center'>Hello Word</h1>
+    <div class='container-fluid'>
+      <table class='table table-bordered'>
+        <thead>
+          <tr>
+            <th scope='col'>#</th>
+            <th scope='col'>First</th>
+            <th scope='col'>Last</th>
+            <th scope='col'>Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope='row'>1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope='row'>2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope='row'>3</th>
+            <td colspan='2'>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <script src='https://code.jquery.com/jquery-3.2.1.slim.min.js'></script>
+    <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js'></script>
   </body>
   </html>
 ";
--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,6 @@
 <?php
   $koneksi = mysqli_connect("127.0.0.1", "root", "root", "db_crud");
-  if ($koneksi) {
-    echo "database konek";
-  } else {
-    echo "database tidak konek";
-  }
+  if (!$koneksi) {
+    echo "tidak konek";
+  } 
 ?>
