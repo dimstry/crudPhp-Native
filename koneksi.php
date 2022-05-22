60b19eedac1488e8bfc6edbf25cf10c7254d01b9
GTM-Winter <dimastriana03@gmail.com>
2022-05-22 07:55:32 +0700

    mengkoneksikan

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$koneksi = mysql_connect("127.0.0.0", "root", "", "db_crud");
-if ($koneksi) {
-  echo "database konek";
-} else {
-  echo "database tidak konek";
-}
+  $koneksi = mysqli_connect("127.0.0.1", "root", "root", "db_crud");
+  if ($koneksi) {
+    echo "database konek";
+  } else {
+    echo "database tidak konek";
+  }
 ?>
